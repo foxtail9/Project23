5621ef85795840faf86770d2bb5bf164b7bef69c
GEUN SEOK <74439615+foxtail9@users.noreply.github.com>
2024-11-06 06:18:19 +0900

    (add)좀비 사운드 추가

--- Assets/Script/Monters/Zombie.cs
+++ Assets/Script/Monters/Zombie.cs
@@ -10,13 +10,20 @@ public enum AIState
 }
 public class Zombie : Monster
 {
+    public AudioClip[] sounds; // 일반 사운드 클립 배열
+    public AudioClip attackSound; // 공격 사운드 클립
+    public float minTimeBetweenSounds = 10f; // 일반 사운드 재생 간 최소 시간
+    public float maxTimeBetweenSounds = 5f; // 일반 사운드 재생 간 최대 시간
     private float lastAttackTime;
+    private float lastSoundTime;
+    private AudioSource audioSource; // AudioSource 컴포넌트
 
-    // Start is called before the first frame update
     public override void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         base.Start();
         SetState(AIState.IdleState);
+        StartCoroutine(PlayRandomSound());
     }
 
     // Update is called once per frame
@@ -38,7 +45,7 @@ public class Zombie : Monster
             case AIState.DeadState:
                 break;
         }
-        
+
     }
 
     public void SetState(AIState state)
@@ -49,7 +56,7 @@ public class Zombie : Monster
 
     public void IdleUpdate()
     {
-        if(RecognizePlayer())
+        if (RecognizePlayer())
         {
             SetState(AIState.MoveState);
         }
@@ -58,11 +65,11 @@ public class Zombie : Monster
     public void MoveUpdate()
     {
         MoveToPlayer();
-        if(!RecognizePlayer())
+        if (!RecognizePlayer())
         {
             SetState(AIState.IdleState);
         }
-        else if(playerDistance <data.attackDistance)
+        else if (playerDistance < data.attackDistance)
         {
             AttackUpdate();
         }
@@ -75,6 +82,31 @@ public class Zombie : Monster
             lastAttackTime = Time.time;
             // player takes damage
             Attack();
+            PlayAttackSound();
+        }
+    }
+    private IEnumerator PlayRandomSound()
+    {
+        while (true) // 무한 루프
+        {
+            // 일반 사운드 재생
+            if (Time.time - lastSoundTime > Random.Range(minTimeBetweenSounds, maxTimeBetweenSounds))
+            {
+                int randomIndex = Random.Range(0, sounds.Length);
+                audioSource.clip = sounds[randomIndex];
+                audioSource.Play();
+                lastSoundTime = Time.time;
+            }
+            yield return null; 
+        }
+    }
+
+    private void PlayAttackSound()
+    {
+        if (attackSound != null)
+        {
+            audioSource.clip = attackSound;
+            audioSource.Play();
         }
     }
 }
