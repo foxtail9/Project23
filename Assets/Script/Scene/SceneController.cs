055ecbbf91d2670ad9abd80c38c2d1e7742387b1
new-Tower-of-Babel <net6994@gmail.com>
2024-11-05 19:59:11 +0900

    [update]TitleSceneButton
    타이틀신 버튼 업데이트
    시작시간 버튼 추가

--- Assets/Script/Scene/SceneController.cs
+++ Assets/Script/Scene/SceneController.cs
@@ -2,13 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
-    public void HospitalSceneLoad()
+    public GameObject playbtn;
+    public GameObject btnAM7;
+    public GameObject btnPM9;
+
+    public void TimeChoiceOn()
+    {
+        playbtn.SetActive(false);
+        btnAM7.SetActive(true);
+        btnPM9.SetActive(true);
+    }
+    public void HospitalSceneLoad_AM7()
     {
         SceneManager.LoadScene("ClosedHospital");
+        GameTime.startTime = 7;
     }
+    public void HospitalSceneLoad_PM9()
+    {
+        SceneManager.LoadScene("ClosedHospital");
+        GameTime.startTime = 21;
+    }
+    
     public void TitleSceneLoad()
     {
         SceneManager.LoadScene("TitleScene");
